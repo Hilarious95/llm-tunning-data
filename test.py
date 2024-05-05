7409f71b1454de0f46ed109524474bf812edb0bb
hilarious <124161130+Hilarious95@users.noreply.github.com>
2024-05-05 22:40:05 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,110 @@
+from unsloth import FastLanguageModel
+import torch
+from datasets import load_dataset, Dataset
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
+import requests
+import json
+
+max_seq_length = 2048
+
+#unsloth support llama-3 by default, you can also use any model available on huggingface
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/llama-3-8b-bnb-4bit",
+    max_seq_length = max_seq_length,
+    dtype = torch.float16,
+    load_in_4bit = True,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+url = "https://raw.githubusercontent.com/Hilarious95/llm-tunning-data/main/input.json"
+
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+pass
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        dataset = load_dataset("json", data_files=url, split="train")
+        dataset = dataset.map(formatting_prompts_func, batched = True,)
+    else:
+        print("Failed to retrieve data. Status code:", response.status_code)
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
+
+
+# Define the PEFT configuration
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    max_seq_length = max_seq_length,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+# Configure Unsloth Trainer
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 10,
+        max_steps = 60,
+        fp16 = True,
+        bf16 = False,
+        logging_steps = 1,
+        optim = "adamw_8bit",
+
+        seed = 3407,
+        output_dir = "outputs",
+    ),
+)
+
+# Memory saved using unsloth:
+#Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+
+
+trainer_stats = trainer.train()
